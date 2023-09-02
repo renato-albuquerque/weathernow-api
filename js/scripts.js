75546e199353bf251d0ed2f4c01cc0e8ce22fea6
Renato Albuquerque <renatomalb83@gmail.com>
2023-09-01 23:32:48 -0300

    review, updated project

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ const getWeatherData = async(city) => {
     return data;
 }
 
-const showWeatherData = async (city) => {
+const showWeatherData = async(city) => {
     const data = await getWeatherData(city);
 
     cityElement.innerText = data.name;
